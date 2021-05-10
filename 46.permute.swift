6b3a8f05e87dff90d557b8a730a320ddbac1c0a2
Hanser <hepengfei@qiyi.com>
2021-05-10 13:33:19 +0800

    全排列1

---
+++ 46.permute.swift
@@ -0,0 +1,56 @@
+/**
+
+ 46. 全排列
+ 给定一个不含重复数字的数组 nums ，返回其 所有可能的全排列 。你可以 按任意顺序 返回答案。
+
+ 示例 1：
+
+ 输入：nums = [1,2,3]
+ 输出：[[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+ 示例 2：
+
+ 输入：nums = [0,1]
+ 输出：[[0,1],[1,0]]
+ 示例 3：
+
+ 输入：nums = [1]
+ 输出：[[1]]
+
+ 提示：
+
+ 1 <= nums.length <= 6
+ -10 <= nums[i] <= 10
+ nums 中的所有整数 互不相同
+
+ */
+
+func backTrack(_ nums: [Int], _ used: inout [Bool], _ combination: inout [Int], _ result: inout [[Int]]) {
+    // if index == nums.count - 1 {
+    if combination.count == nums.count {
+        result.append(combination)
+        return
+    }
+    // return
+    // }
+
+    for i in 0 ..< nums.count {
+        if used[i] { continue }
+        used[i] = true
+        combination.append(nums[i])
+        backTrack(nums, &used, &combination, &result)
+        combination.removeLast()
+        used[i] = false
+    }
+}
+
+func permute(_ nums: [Int]) -> [[Int]] {
+    var used = Array(repeating: false, count: nums.count)
+    var result = [[Int]]()
+    var combination = [Int]()
+
+    backTrack(nums, &used, &combination, &result)
+    return result
+}
+
+let res = permute([1, 2, 3])
+print(res)
